427704cbe48d1902444f00091c5ad846ec7958b7
Jaapster1337 <ejboutens1337@gmail.com>
2019-06-17 19:07:58 +0200

    Adding files

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+let randomNumber = Math.floor(Math.random()*100) +1;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('/lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
