20f851ff487627eafdc66149d10111fbbeaa6fcf
Jaapster1337 <ejboutens1337@gmail.com>
2019-06-17 19:26:50 +0200

    removed button, added function 'checkGuess'

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ myHeading.textContent = 'Hello world!';
 let randomNumber = Math.floor(Math.random()*100) +1;
 
 const guesses = document.querySelector('.guesses');
-const lastResult = document.querySelector('/lastResult');
+const lastResult = document.querySelector('lastResult');
 const lowOrHi = document.querySelector('.lowOrHi');
 
 const guessSubmit = document.querySelector('.guessSubmit');
@@ -12,3 +12,12 @@ const guessField = document.querySelector('.guessField');
 
 let guessCount = 1;
 let resetButton;
+
+<label for="guessField">Enter a guess: <label><input type="text" id="guessField" class="guessField">;
+<input type="submit" value="Submit guess" class="guessSubmit">
+
+function checkGuess(){
+	alert('placeholder');
+}
+
+checkGuess();
